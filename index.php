d78b1f2df0fbd0d5d8b48ff824dfe27208ccc9af
Daniel Ryan <d@dryan.com>
2018-05-07 08:36:37 -0400

    debugging

--- index.php
+++ index.php
@@ -120,8 +120,8 @@
     die();
   endif;
 
-  if(count($parts) < 2):
-    header('X-404: count($parts) < 2 ' . json_encode($parts));
+  if(count($parts) < 1):
+    header('X-404: count($parts) < 1 ' . json_encode($parts));
     header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
     echo file_get_contents('404.html');
     die();
